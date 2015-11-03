265af0f2ccfadb239e7b77cad4f16a68fef85990
Dmitry E. Kislov <kislov@easydan.com>
2015-11-04 00:00:18 +1000

    Clearing temporary files when testing

--- django_solver/django_solver/test/test_taskmodel.py
+++ django_solver/django_solver/test/test_taskmodel.py
@@ -1,15 +1,21 @@
 import os
+import shutil
 import tempfile
-from django.test import TestCase
-from ..models import TaskModel, PythonCodeModel, TemplateModel
-from django.core.files import File
+
+from django.conf import settings
 from django.core.exceptions import ValidationError
-# from django.test.utils import override_settings
+from django.core.files import File
+from django.test import TestCase, override_settings
 
-
+from ..models import TaskModel, PythonCodeModel, TemplateModel
 from .data import template_data
 
+
+# from django.test.utils import override_settings
 current_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+MROOT = os.path.abspath(getattr(settings, 'MEDIA_ROOT', ''))
+NEWMROOT = tempfile.mkdtemp(dir=MROOT)
 
 
 def _safely_create(model, filepath, filefield='file'):
@@ -21,7 +27,7 @@ def _safely_create(model, filepath, filefield='file'):
         return
     return obj
 
-
+@override_settings(MEDIA_ROOT=NEWMROOT)
 class TemplateModelTestCase(TestCase):
 
     def setUp(self):
@@ -32,6 +38,9 @@ class TemplateModelTestCase(TestCase):
                        template_data.VALID_TEMPLATE_FILENAME_JINJA)
                        )
 
+    def tearDown(self):
+        shutil.rmtree(NEWMROOT, ignore_errors=True)
+
     def test_body_creation(self):
         self.assertEqual(TemplateModel.objects.exclude(body__exact='')[0].body,
                          template_data.VALID_TEMPLATE_BODY_JINJA)
@@ -41,18 +50,20 @@ class TemplateModelTestCase(TestCase):
                       TemplateModel.objects.get(body__exact='').file.name,
                       )
 
-
+@override_settings(MEDIA_ROOT=NEWMROOT)
 class PythonCodeTestCase(TestCase):
     def setUp(self):
         PythonCodeModel.objects.create(
                         body=template_data.VALID_PYTHON_CODE
                                        )
 
+    def tearDown(self):
+        shutil.rmtree(NEWMROOT, ignore_errors=True)
+
     def test_valid_python_code_body(self):
         self.assertEqual(PythonCodeModel.objects.all()[0].body,
                          template_data.VALID_PYTHON_CODE)
-    
-#     @override_settings(STATIC_ROOT='/other/login/')
+
     def test_valid_python_code_file(self):
         _safely_create(PythonCodeModel, os.path.join(current_dir, 'test', 'data',
                        template_data.VALID_PYTHON_CODE_FILENAME)
