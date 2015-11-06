b22c8d968c6fdd408149c8586e2ae3a714c152d7
Dmitry E. Kislov <kislov@easydan.com>
2015-11-06 20:51:58 +1000

    Tweak testrunner for the project

--- django_solver/django_solver/tests/runtests.py
+++ django_solver/django_solver/tests/runtests.py
@@ -11,5 +11,5 @@ if __name__ == "__main__":
     django.setup()
     TestRunner = get_runner(settings)
     test_runner = TestRunner()
-    failures = test_runner.run_tests(["tests.tests"])
+    failures = test_runner.run_tests(["tests.test_taskmodel"])
     sys.exit(bool(failures))
