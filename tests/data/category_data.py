ed5fb8d4804f693a4786942a76a1658cf7cda050
Kislov Dmitry <kislov@easydan.com>
2015-11-13 15:02:25 +1000

    Category testing data included

---
+++ tests/data/category_data.py
@@ -0,0 +1,24 @@
+
+test_category_keys = 'one, two, main apple, three'
+
+test_category_name = 'My test category'
+
+test_category_descr = 'This is a useful test category....'
+
+test_child1_name = 'Child 1'
+test_child2_name = 'Child 2'
+test_child3_name = 'Child 3'
+test_child4_name = 'Child 4'
+
+
+# ------ It is assumed the following test category Structure -----
+#
+#        base_category
+#        /           \
+#       /             \   
+#      Child 1         Child 2  
+#                        /  \ 
+#                       /    \ 
+#                    Child3  Chield4
+# -----------------------------------------------------------------
+
