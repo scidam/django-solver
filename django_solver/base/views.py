197d1f9dda4c4e0f10b96e004c9165d520120246
Kislov Dmitry <kislov@easydan.com>
2015-12-08 16:20:55 +1000

    empty function for task loading

---
+++ django_solver/base/views.py
@@ -0,0 +1,26 @@
+
+
+
+# -------------------- TASK STACK -------------------------
+# 1) TASK-STACK is an array
+# 2) TASK-STACK stored in database or redis
+# 3) In order to put to task-stack, the problem should pass validation process
+
+
+
+
+    
+def load_data():
+    pass
+
+
+
+def load_task(request):
+    '''
+    Validate, load and append to solution stack a task
+    '''
+    
+    
+
+def check_status(request):
+    pass
